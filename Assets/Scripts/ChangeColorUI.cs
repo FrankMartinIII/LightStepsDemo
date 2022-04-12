864dba6a21bd99b74ccd6c63633e4e8fedd161b5
Hannah Chin <hchin157@gmail.com>
2022-04-12 00:50:58 -0400

    Color Change UI
    Added UI that indicates current color as well as "adjacent colors," so that the player knows which color they are switching to

---
+++ Assets/Scripts/ChangeColorUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class ChangeColorUI : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] spriteArray;
+    public GameManager gameManager;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int cc = (int)gameManager.curColor;
+        spriteRenderer.sprite = spriteArray[cc-1];
+    }
+    
+}
